3e15000659c6081e5769e4d61147f97f11aadbcf
meshir78 <meshir78@uwosh.edu>
2016-12-09 01:23:34 -0600

    ok photos are now officially submitting and loading from db

--- upload.php
+++ upload.php
@@ -1,17 +1,19 @@
 <?php
+    error_reporting(E_ERROR | E_PARSE);
+
     $target_dir = "uploads/";
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
  
     $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
     // Check if image file is a actual image or fake image
     if(isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-        if($check !== false) {
-            if($imageFileType == "jpg" || $imageFileType == "png" || $imageFileType == "jpeg"
+        if($imageFileType == "jpg" || $imageFileType == "png" || $imageFileType == "jpeg"
 || $imageFileType == "gif" ) {
-                move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
-                header("Location:http://webdev.cs.uwosh.edu/students/meshir78/HotDogCharlies/gallery.php");
-}
+            move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
+            $db = new PDO("mysql:dbname=meshir78;host=localhost", "meshir78", "rmesh0484278");
+            $rows = $db->exec("INSERT INTO `Gallery`(`image`) VALUES ('$target_file')");
         }
     }
+        header("Location:http://webdev.cs.uwosh.edu/students/meshir78/HotDogCharlies/gallery.php");
+   
 ?>
